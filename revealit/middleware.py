1dfe57ba7e7c3297d68b7c9d5271ec98dba0d93f
Mikkel Hoegh <mikkel@hoegh.org>
2010-01-04 20:40:19 +0100

    Adding SpacelessMiddleware and setup.py.

--- revealit/middleware.py
+++ revealit/middleware.py
@@ -1,4 +1,19 @@
 """
 Django middlewares used by Reveal IT.
 """
+from django.utils.html import strip_spaces_between_tags
+
+
+class SpacelessMiddleware(object):
+    """
+    Middleware to remove excess whitespace.
+
+    By David Cramer - taken from:
+        http://www.davidcramer.net/code/369/spaceless-html-in-django.html
+    """
+    def process_response(self, request, response):
+        if 'text/html' in response['Content-Type']:
+            response.content = strip_spaces_between_tags(response.content)
+        return response
+
 
---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='libreveal',
+    version='0.1',
+    description='Reveal IT’s collection of Django snippets.',
+    author='Mikkel Høgh',
+    author_email='mikkel@revealit.dk'
+    packages=['revealit'],
+)
+

4ab665a4726e34c8e98362524511f2601524517e
Mikkel Hoegh <mikkel@hoegh.org>
2010-01-04 20:42:31 +0100

    Fix syntax error.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     description='Reveal IT’s collection of Django snippets.',
     author='Mikkel Høgh',
-    author_email='mikkel@revealit.dk'
+    author_email='mikkel@revealit.dk',
     packages=['revealit'],
 )
 
